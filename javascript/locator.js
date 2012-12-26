3db6145bb41807d29e0fa7cf9f2a3b71c8d08fa0
Andrew67 <andres@andrew67.com>
2012-12-26 18:20:45 -0400

    Added disclaimer that data may be out of date, and a link to zenius-i-vanisher when no results are found. Removed HTML fragment from JS code corresponding to a list item; instead have a hidden dummy item in the HTML that gets cloned with data modified dynamically. Added working support for Navigate button that appears in Android, iPhone/iPad and Windows Phone. Removed (beta) tag. Modified layout to have buttons be in their own paragraphs (better separation between error message and action buttons). Reformatted parts of HTML code that exceeded the recommended 120-character line limit, for improved readability.

--- javascript/locator.js
+++ javascript/locator.js
@@ -1,5 +1,16 @@
 // Funcationality for locator page
 $(window).load(function () {
+    // Prefixes for arcade item info/navigation links
+    var GMAPS_PREFIX = 'https://maps.google.com/?q=loc:';
+    var ZIV_PREFIX = 'http://m.zenius-i-vanisher.com/arcadelocations_viewarcade.php?locationid=';
+    var NAV_PREFIX_ANDROID = 'geo:0,0?q=loc:';
+    var NAV_PREFIX_IOS = 'maps:?q=&saddr=Current%20Location&daddr=loc:';
+    var NAV_PREFIX_WP7 = 'bingmaps:?q=';
+    var NAV_PREFIX = '#';
+    if (/Android/i.test(navigator.userAgent)) NAV_PREFIX = NAV_PREFIX_ANDROID;
+    else if (/(iPhone)|(iPad)/i.test(navigator.userAgent)) NAV_PREFIX = NAV_PREFIX_IOS;
+    else if (/Windows Phone/i.test(navigator.userAgent)) NAV_PREFIX = NAV_PREFIX_WP7;
+
     // Geolocation error handler
     var handle_error = function(error) {
         // Permission denied
@@ -13,6 +24,7 @@ $(window).load(function () {
             $('#message-failed').show();
         }
     };
+
     // Arcade location data handler function
     var handle_data = function(locations) {
         $('#message-found-searching').hide();
@@ -23,19 +35,25 @@ $(window).load(function () {
             $('#arcade-noresults-container').show();
         }
         else {
+            // Grab the item layout element
+            var arcade_list_item = $('.arcade-list-item:first-child');
+            // For each location found, clone the main layout, fit in the details and add it to the list
             for (var i = 0; i < locations.length; ++i) {
-                arcade_list.append('<li><a href="#">' + locations[i].name + '</a><div><h3>'
-                    + locations[i].city + '</h3><h4>Approximately '
-                    + locations[i].distance + 'km away</h4><a class="button bg-color-green fg-color-white" href="https://maps.google.com/?q=loc:'
-                    + locations[i].latitude + ',' + locations[i].longitude + '">Find on Google Maps <i class="icon-location-2"></i></a><br>'
-                    + '<a class="button bg-color-blueDark fg-color-white" href="http://m.zenius-i-vanisher.com/arcadelocations_viewarcade.php?locationid='
-                    + locations[i].id + '">Info on Zenius -I- vanisher.com <i class="icon-new-tab"></i></a>'
-                    + '</div></li>');
+                var arcade = arcade_list_item.clone();
+                arcade.find('.arcade-name').text(locations[i].name);
+                arcade.find('.arcade-city').text(locations[i].city);
+                arcade.find('.arcade-distance').text(locations[i].distance);
+                var mapsuffix = locations[i].latitude + ',' + locations[i].longitude + '(' + encodeURI(locations[i].name) + ')';
+                arcade.find('.arcade-nav').attr('href', NAV_PREFIX + mapsuffix);
+                arcade.find('.arcade-gmaps').attr('href', GMAPS_PREFIX + mapsuffix);
+                arcade.find('.arcade-ziv').attr('href', ZIV_PREFIX + locations[i].id);
+                arcade.appendTo(arcade_list);
             }
             // Load accordion library after preparing list, because it attaches click events to list elements themselves
             $('body').append('<script src="javascript/accordion.js"><\/script>');
         }
     };
+
     // Geolocation ok handler
     var handle_ok = function(position) {
         $('#message-waiting').hide();
@@ -51,6 +69,7 @@ $(window).load(function () {
         // Locate nearby machines and populate/show list
         $.getJSON('locate.php', {'lat': position.coords.latitude, 'long': position.coords.longitude, 'timestamp': position.timestamp}, handle_data);
     };
+
     // Geolocation feature detection from Modernizr
     if ('geolocation' in navigator) {
         $('#message-loading').hide();
