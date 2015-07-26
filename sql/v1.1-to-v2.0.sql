3144e482ab127c3785a3988373a7477b91b967ac
Andrew67 <andres@andrew67.com>
2015-07-26 13:54:15 -0400

    Change source_type and source_id to varchar (latter in order to preserve id as provided by source for URL substitution).

--- sql/v1.1-to-v2.0.sql
+++ sql/v1.1-to-v2.0.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `locations` ALTER COLUMN `city` SET DEFAULT '';
 ALTER TABLE `locations` ADD COLUMN `last_update` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `locations` ADD COLUMN `source_type` char(8) NOT NULL AFTER `id`;
-ALTER TABLE `locations` ADD COLUMN `source_id` int(11) NOT NULL AFTER `source_type`;
+ALTER TABLE `locations` ADD COLUMN `source_type` varchar(8) NOT NULL AFTER `id`;
+ALTER TABLE `locations` ADD COLUMN `source_id` varchar(16) NOT NULL AFTER `source_type`;
 UPDATE `locations` SET `source_id`=`id`, `source_type`='ziv';
 CREATE UNIQUE INDEX `composite_id` ON `locations` (`source_type`, `source_id`);
 CREATE INDEX `source_coordinates` ON `locations` (`source_type`, `latitude`, `longitude`);
--- sql/v2.0.sql
+++ sql/v2.0.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `locations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `source_type` char(8) NOT NULL,
-  `source_id` int(11) NOT NULL,
+  `source_type` varchar(8) NOT NULL,
+  `source_id` varchar(16) NOT NULL,
   `name` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL DEFAULT '',
   `latitude` double NOT NULL,
