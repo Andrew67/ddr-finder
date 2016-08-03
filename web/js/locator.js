b1a2ab8eb235446f409bb5b70f7a07ab9989c675
Andrew67 <andres@andrew67.com>
2016-08-02 23:24:45 -0400

    Created new MapBuilder abstraction over the Static Maps API, which allowed adding the closest 5 arcades as pins on the "Your Location" map.

--- web/js/locator.js
+++ web/js/locator.js
@@ -1,8 +1,21 @@
 /*! ddr-finder | https://github.com/Andrew67/ddr-finder/blob/master/LICENSE */
 // Funcationality for locator page
 $(window).load(function () {
-    // Google Maps Static API Key
-    var GMAPS_API_KEY = 'AIzaSyAek3wRV_aVi5ZPR8tkI4WxsGcVZjz8MaE';
+    // Google Maps Static API builder
+    function MapBuilder() {
+        // Google Maps Static API Key
+        var GMAPS_API_KEY = 'AIzaSyAek3wRV_aVi5ZPR8tkI4WxsGcVZjz8MaE';
+        this.url = 'https://maps.google.com/maps/api/staticmap?size=288x216&key='+GMAPS_API_KEY;
+        this.nextMarkerNumber = 1;
+    }
+    MapBuilder.prototype.getURL = function() { return this.url; };
+    MapBuilder.prototype.addMyLocationMarker = function(coords) { // Coords in "lat,lng" format
+        this.url += '&markers='+coords;
+    };
+    MapBuilder.prototype.addMarker = function(coords) { // Warning: Static Maps API limited to 9 for labels
+        this.url += '&markers=color:blue|label:'+(this.nextMarkerNumber++)+'|'+coords;
+    };
+    var locationMap = new MapBuilder();
 
     // Prefixes for arcade item navigation links
     var GMAPS_PREFIX = 'https://maps.google.com/?q=loc:';
@@ -77,7 +90,7 @@ $(window).load(function () {
         else {
             // Grab the item layout element
             var arcade_list_item = $('.arcade-list-item:first-child');
-            // For each location found, clone the main layout, fit in the details and add it to the list
+            // For each location found, clone the main layout, fill in the details, and add it to the list
             for (var i = 0; i < locations.length; ++i) {
                 var arcade = arcade_list_item.clone();
                 arcade.find('.arcade-name').text(locations[i].name);
@@ -94,6 +107,11 @@ $(window).load(function () {
                 arcade.find('.arcade-info').attr('href',
                     info_url(data.sources, locations[i].src, locations[i].id, locations[i].sid));
                 arcade.appendTo(arcade_list);
+
+                // The first 5 locations get added to the "Your Location" mini-map as well
+                if (i < 5) {
+                    locationMap.addMarker(locations[i].lat + ',' + locations[i].lng);
+                }
             }
             // Execute accordion function on list manually after populating it,
             // since the library attaches click events to the list items themselves (attaching to nothing on page load)
@@ -107,16 +125,18 @@ $(window).load(function () {
         $('#message-api-failed').show();
     };
 
+    // Load location map based on builder so far
+    var load_location_map = function() {
+        $('#current-location-img').attr('src', locationMap.getURL());
+    };
+
     // Geolocation ok handler
     var handle_ok = function(position) {
         $('#message-waiting').hide();
         $('#message-found-searching').show();
         var coords = '' + position.coords.latitude + ',' + position.coords.longitude;
         $('#current-location-link').attr('href', 'https://maps.google.com/maps?q='+coords+'&ll='+coords+'&z=16&t=h');
-        $('#current-location-img').attr(
-            'src',
-            'https://maps.google.com/maps/api/staticmap?center='+coords+'&zoom=16&size=288x216&markers='+coords+'&key='+GMAPS_API_KEY
-        );
+        locationMap.addMyLocationMarker(coords);
         var accuracy = (position.coords.accuracy >= 1000) ?
             '' + (position.coords.accuracy / 1000).toFixed(2) + 'km' :
             '' + position.coords.accuracy.toFixed() + ' meters';
@@ -128,7 +148,7 @@ $(window).load(function () {
             'datasrc': datasrc,
             'lat': position.coords.latitude,
             'lng': position.coords.longitude
-        }).done(handle_data).fail(handle_data_error);
+        }).done(handle_data).fail(handle_data_error).always(load_location_map);
     };
 
     // Geolocation feature detection from Modernizr
