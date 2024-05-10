df938f327f9b2f44a54030931098254c09bb1cc7
Andrew67 <andres@andrew67.com>
2024-05-10 00:40:16 -0400

    Bug: distanceKm should be part of the properties object of an ArcadeLocationWithDistance

--- docs/v4/nearby.d.ts
+++ docs/v4/nearby.d.ts
@@ -5,11 +5,13 @@ import type { ArcadeLocation, ArcadeLocationApiResponse } from './all.d.ts';
  */
 
 export interface ArcadeLocationWithDistance extends ArcadeLocation {
-  /**
-   * Approximate distance to this location from the coordinates provided to the search query, in kilometers.
-   * Maximum precision of 2 decimal digits.
-   */
-  distanceKm: number;
+  properties: ArcadeLocation['properties'] & {
+    /**
+     * Approximate distance to this location from the coordinates provided to the search query, in kilometers.
+     * Maximum precision of 2 decimal digits.
+     */
+    distanceKm: number;
+  }
 }
 
 export interface NearbyApiResponse extends ArcadeLocationApiResponse<ArcadeLocationWithDistance> {
